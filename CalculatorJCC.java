03f1c54c4ca2110143e9c4e690412b934ef00376
Houssen Yang <yanghoussen@gmail.com>
2024-12-12 04:43:08 -0500

    Added calculator template

---
+++ CalculatorJCC.java
@@ -0,0 +1,66 @@
+public class CalculatorJCC {
+    public static void main(String[] args) {
+        // Declare a string variable called "operation"
+        // Declare a double variable called "operand1"
+        // Declare a double variable called "operand2"
+        // Declare a double variable called "result"
+
+        // Initailze a boolean variable called "exitCommandNotEntered" to "true"
+
+        // Write a while loop to run while "exitCommandNotEntered" is true
+            // Prompt the user to "Please enter an operation (ADD, SUB, MUL, DIV): " and assign the value to "operation"
+            // Prompt the user to enter in the first operand and assign the value to "operand1"
+            // Prompt the user to enter in the second operand and assign the value to "operand2"
+
+            // Write an if-statement to see if the operation was "ADD"
+                // Call the "add" function and assign the return value to "result"
+                // Print "result" to the user
+            // Else if the operation was "SUB"
+                // Call the "subtract" function and assign the return value to "result"
+                // Print "result" to the user
+            // Else if the operation was "MUL"
+                // Call the "multiply" function and assign the return value to "result"
+                // Print "result" to the user
+            // Else if the operation was "DIV"
+                // Call the "divide" function and assign the return value to "result"
+                // Print "result" to the user
+            // Else
+                // Print to the user "Command not found!" 
+
+            // Prompt the user "Exit (Y/N): " and assign the value to "operation"
+            // Write an if-statement to see if the operation was "Y"
+                // Assign "exitCommandNotEntered" to "false"
+    }
+
+    // Define a function called "add" that takes operand1 and operand2
+    // and returns their sum.
+    //     Input Arguments:
+    //        - operand1 : double
+    //        - operand2 : double
+    //     Output Return Values:
+    //        - sum : double
+
+    // Define a function called "subtract" that takes operand1 and operand2
+    // and returns their difference.
+    //     Input Arguments:
+    //        - operand1 : double
+    //        - operand2 : double
+    //     Output Return Values:
+    //        - difference : double
+
+    // Define a function called "multiply" that takes operand1 and operand2
+    // and returns their product.
+    //     Input Arguments:
+    //        - operand1 : double
+    //        - operand2 : double
+    //     Output Return Values:
+    //        - product : double
+
+    // Define a function called "divide" that takes operand1 and operand2
+    // and returns their quotient.
+    //     Input Arguments:
+    //        - operand1 : double
+    //        - operand2 : double
+    //     Output Return Values:
+    //        - quotient : double
+}
